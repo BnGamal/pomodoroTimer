33fddf405ba671dad9d8806351262b386948e557
BnGamal <isl1129630i@gmail.com>
2024-03-14 17:54:12 -0700

    initial files

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+const daysInputFile = document.getElementsBy('days');
+const hoursInputEle = document.getElementById('hours');
+const minutesInputEle = document.getElementsBy('minutes');
+const secondsInputEle = document.getElementsBy('seconds');
+
+let timeLeft = [];
+
+
+
+const confirmDurationBtn = document.getElementsBy('confirm');
+
