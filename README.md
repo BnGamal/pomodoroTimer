5adc930b27c82ab983c3c08afcdf4fe5115572fc
BnGamal <isl1129630i@gmail.com>
2024-03-17 23:35:05 -0700

    basic user interface

---
+++ README.md
@@ -0,0 +1,15 @@
+# Styles:
+- [ ] Y2K
+- [ ] Minimal
+- [ ] Skeuomorphic
+- [ ] Glass Morphic
+- [ ] Grainy
+- [ ] Modern 
+
+
+# urgent improvements:
+- change 'welcome' in landing to 'you're running out of time'
+
+# improvements
+- choose a pattern to be greeted with everytime you login
+- 
--- main.js
+++ main.js
@@ -2,16 +2,138 @@
 
 
 
+const firstShape = { // click
+	"2": [1,3,5],
+	"3": [2,3,4],
+	"4": [1,2,4,5],
+	"5": [2,3,4],
+	"6": [1,3,5],};
 
+const bluePrints = [
+{ // 0
+	"1": [2,3,4],
+	"2": [1,5],
+	"3": [1,5],
+	"4": [1,5],
+	"5": [1,5],
+	"6": [1,5],
+	"7": [2,3,4]
+},
+{ // 1
+	"1": [3],
+	"2": [2,3],
+	"3": [3],
+	"4": [3],
+	"5": [3],
+	"6": [3],
+	"7": [2,3,4]
+},
+{ // 2
+	"1": [2,3,4],
+	"2": [1,5],
+	"3": [5],
+	"4": [4],
+	"5": [3],
+	"6": [2],
+	"7": [1,2,3,4,5]
+},
+{ // 3
+	"1": [2,3,4],
+	"2": [1,5],
+	"3": [5],
+	"4": [2,3,4],
+	"5": [5],
+	"6": [1,5],
+	"7": [2,3,4]
+},
+{ // 4
+	"1": [3,4],
+	"2": [2,4],
+	"3": [1,4],
+	"4": [1,2,3,4,5],
+	"5": [4],
+	"6": [4],
+	"7": [4]
+},
+{ // 5
+	"1": [1,2,3,4,5],
+	"2": [1],
+	"3": [1,2,3,4],
+	"4": [5],
+	"5": [5],
+	"6": [1,5],
+	"7": [2,3,4]
+},
+{ // 6
+	"1": [2,3,4],
+	"2": [1,5],
+	"3": [1],
+	"4": [1,2,3,4],
+	"5": [1,5],
+	"6": [1,5],
+	"7": [2,3,4]
+},
+{ // 7
+	"1": [1,2,3,4,5],
+	"2": [5],
+	"3": [4],
+	"4": [3],
+	"5": [3],
+	"6": [3],
+	"7": [3]
+},
+{ // 8
+	"1": [2,3,4],
+	"2": [1,5],
+	"3": [1,5],
+	"4": [2,3,4],
+	"5": [1,5],
+	"6": [1,5],
+	"7": [2,3,4]
+},
+{ // 9
+	"1": [2,3,4],
+	"2": [1,5],
+	"3": [1,5],
+	"4": [2,3,4,5],
+	"5": [5],
+	"6": [1,5],
+	"7": [2,3,4]
+}];
 
-const daysInputFile = document.getElementsBy('days');
-const hoursInputEle = document.getElementById('hours');
-const minutesInputEle = document.getElementsBy('minutes');
-const secondsInputEle = document.getElementsBy('seconds');
+const screensContainer = document.getElementById('screens-container');
 
-let timeLeft = [];
+function getPairs(obj) {
+	keys = Object.keys(obj);
+	for (const yCoordinate of keys) {
+		xCoordinates = obj[yCoordinate];
+		for (const xCoordinate  of xCoordinates) {
+			const id = (Number(yCoordinate) *  7) + xCoordinate + 1;
+			const screen = screensContainer.querySelector('#screen' + id);
+			setTimeout(function() {
+				screen.classList.add('flip');
+			}, 10);
+			setTimeout(function() {
+				screen.classList.add('color');
+			}, 15)
+		}
+	}
+}
 
+function clearScreens() {
+	const allScreens = document.querySelectorAll('span');
+	for (screen of allScreens) {
+		if (screen.classList.contains('flip'));
+		screen.classList.remove('flip', 'color');
+	}
+}
 
+const pullUpMenu = document.getElementById('pull-up-menu');
+const pullUpMenuBtn = document.getElementById('pull-up-menu-btn');
 
-const confirmDurationBtn = document.getElementsBy('confirm');
+pullUpMenuBtn.addEventListener('click', function() {
+	pullUpMenu.classList.toggle('move-menu');
+	pullUpMenuBtn.classList.toggle('move-button');
+});
 
+getPairs(firstShape);
